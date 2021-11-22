429b32a08f1312c6a8bda5b3338b0f74086bfb27
vagrant <wong_michael@rogers.com>
2021-11-22 03:44:18 +0000

    Finish spotter 2

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('93.28.202.218', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log("It worked! Returned coordinates:" , coordinates);
 });
--- iss.js
+++ iss.js
@@ -1,5 +1,5 @@
+//ISS Spotter I
 const request = require('request');
-
 /**
  * Makes a single API request to retrieve the user's IP address.
  * Input:
@@ -10,24 +10,57 @@ const request = require('request');
  * it should pass through the error to the callback if an error occurs when requesting the IP data
 * parse and extract the IP address using JSON and then pass that through to the callback (as the second argument) if there is no error
  */
-const fetchMyIP = function(callback) {
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-    //inside the request callback
-    //error can be set if invalid comain, user is offline
-    if (error) return callback(error, null);
+// const fetchMyIP = function(callback) {
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+//     //inside the request callback
+//     //error can be set if invalid comain, user is offline
+//     if (error) return callback(error, null);
 
-    //If non-200 status, assume server error
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+//     //If non-200 status, assume server error
+//     if (response.statusCode !== 200) {
+//       callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+//       return;
+//     }
+
+//     //if we get here, all's well and we got the data
+
+//     const ip = JSON.parse(body).ip;
+//     callback(null, ip);
+//   });
+// };
+
+
+// module.exports = { fetchMyIP };
+
+//ISS Spotter II
+// iss.js
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+
+ const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
       return;
     }
 
-    //if we get here, all's well and we got the data
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
 
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
   });
 };
 
-module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
 
